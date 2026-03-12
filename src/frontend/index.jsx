6ad2f83149d19fdf4785e6039e54f6df003bdc5b
Andriy Pelykh <andrew.pelykh@gmail.com>
2026-03-12 14:01:19 +0200

    Fetch JIRA issue comments

--- src/frontend/index.jsx
+++ src/frontend/index.jsx
@@ -1,18 +1,39 @@
-import React, { useEffect, useState } from "react";
-import ForgeReconciler, { Text } from "@forge/react";
-import { invoke } from "@forge/bridge";
+import React from "react";
+// useProductContext hook retrieves current Atlassian app context
+import ForgeReconciler, { Text, useProductContext } from "@forge/react";
+// requestJira calls the Jira REST API
+import { requestJira } from "@forge/bridge";
 
 const App = () => {
-  const [data, setData] = useState(null);
-  useEffect(() => {
-    invoke("getText", { example: "my-invoke-variable" }).then(setData);
-  }, []);
-  return (
-    <>
-      <Text>Hello world everyone!</Text>
-      <Text>{data ? data : "Loading..."}</Text>
-    </>
-  );
+  const context = useProductContext();
+
+  // add the the 'comments' variable to store comments data
+  const [comments, setComments] = React.useState();
+  console.log(`Number of comments on this issue: ${comments?.length}`);
+
+  // start of function that calls Jira REST API
+  const fetchCommentsForIssue = async () => {
+    // extract issue ID instead expecting one from function input
+    const issueId = context?.extension.issue.id;
+    // modify to take issueId variable
+    const res = await requestJira(`/rest/api/3/issue/${issueId}/comment`);
+    const data = await res.json();
+    return data.comments;
+  };
+
+  React.useEffect(() => {
+    if (context) {
+      // extract issue ID from the context
+      const issueId = context.extension.issue.id;
+      // use the issue ID to call fetchCommentsForIssue(),
+      // then updates data stored in 'comments'
+      fetchCommentsForIssue().then(setComments);
+    }
+  }, [context]);
+
+  // This UI will be updated in the next part of this tutorial
+  // to display number of comments onto the screen
+  return <Text>Hello world!</Text>;
 };
 
 ForgeReconciler.render(
